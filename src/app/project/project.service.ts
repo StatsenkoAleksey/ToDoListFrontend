6749314c7025abe792c124e37b7f4f1c87b4f182
StatsenkoAleksey <64027378+StatsenkoAleksey@users.noreply.github.com>
2021-07-02 11:12:21 +0300

    Update project.service.ts

--- src/app/project/project.service.ts
+++ src/app/project/project.service.ts
@@ -1,22 +1,23 @@
-import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
-import { SERVER_URL, LOCAL_URL } from "./project";
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { SERVER_URL, LOCAL_URL } from './project';
+import { HttpHeaders } from '@angular/common/http';
 
 const httpOptions = {
   headers: new HttpHeaders({
     'Content-Type':  'application/json'
   })
-}
+};
 
 @Injectable({
-  providedIn: "root"
+  providedIn: 'root',
 })
 export class ProjectService {
   constructor(private http: HttpClient) {}
 
   getProjects() {
     return this.http.get<Object[]>(SERVER_URL+'/projects')
-    // return this.http.get<Object[]>(LOCAL_URL)
+    // return this.http.get<Object[]>(LOCAL_URL);
   }
 
   patchProject(id: number, projectId: number) {
@@ -33,5 +34,4 @@ export class ProjectService {
       );
     console.log(`${SERVER_URL}/projects/${projectId}/todos/${id}`);
   }
-
 }
